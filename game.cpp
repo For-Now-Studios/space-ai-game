9f76c9628dd5c56b9578e49032dfc98b02b4fb2f
Oriaf <arox99@hotmail.se>
2021-06-15 09:58:31 +0200

    It works!

---
+++ game.cpp
@@ -0,0 +1,87 @@
+#include<SDL2/SDL.h>
+#include<SDL2/SDL_image.h>
+#include<SDL2/SDL_mixer.h>
+#include<SDL2/SDL_ttf.h>
+#include<stdlib.h>
+#include<stdio.h>
+
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+struct WindowStruct {
+	SDL_Window *window;
+	SDL_Texture *screenTexture;
+	SDL_Renderer *render;
+};
+
+/*
+	Initialize the things needed for the engine to run
+*/
+bool init(WindowStruct *window) {
+	//Initialize the SDL library and check if it didn't work, otherwise continue
+	if (SDL_Init(SDL_INIT_VIDEO|SDL_INIT_AUDIO) < 0) {
+		printf("SDL couldn't be initialized. Error: %s\n", SDL_GetError());
+
+		return false;
+	}
+
+	//Create the program window
+	window->window = SDL_CreateWindow("Game", SDL_WINDOWPOS_UNDEFINED,
+		SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+	if (window == NULL) {
+		printf("Couldn't create a window. Error: %s\n", SDL_GetError());
+
+		return false;
+	}
+	//Initialize the renderer
+	window->render = SDL_CreateRenderer(window->window, -1,
+							SDL_RENDERER_ACCELERATED);
+	if (window->render == NULL) {
+		printf("Renderer could not be created! SDL Error: %s\n", SDL_GetError());
+
+		return false;
+	}
+
+	//Set the background colour of the window
+	SDL_SetRenderDrawColor(window->render, 102, 102, 102, 255);
+
+	//Initialize Sound Drivers
+	if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) < 0) {
+		printf("SDL_Mixer could not initialize! SDL_Mixer Error: %s\n",
+									Mix_GetError());
+
+		return false;
+	}
+
+	//Initialize Fonts
+	if (TTF_Init() == -1) {
+		printf("SDL_ttf could not initialize! SDL_ttf Error: %s\n", TTF_GetError());
+
+		return false;
+	}
+
+	//Initialize PNG loading
+	int imgFlags = IMG_INIT_PNG;
+	if (!(IMG_Init(imgFlags) & imgFlags)) {
+		printf("SDL_image could not initialize! SDL_image Error: %s\n",
+									IMG_GetError());
+
+		return false;
+	}
+	
+	return true;
+}
+
+int main(){
+	WindowStruct window;
+
+	if(!init(&window)){
+		exit(1);
+	}
+
+	// The Loop
+	while(true){
+	}
+
+	return 0;
+}
