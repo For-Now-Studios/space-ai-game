4119836dea44ac5654c2dc698ebc0d9232fceb07
Oriaf <arox99@hotmail.se>
2021-06-18 17:15:45 +0200

    Did some minor last tweaks

--- globals.h
+++ globals.h
@@ -1,2 +1,6 @@
-// SOUND
+// SCREEN PROPERTIES
+extern const int SCREEN_WIDTH; //declared in game.cpp
+extern const int SCREEN_HEIGHT; //declared in game.cpp
+
+// SOUND PROPERTIES
 extern int masterVolume; //The baseline volume, declared in sound.cpp
