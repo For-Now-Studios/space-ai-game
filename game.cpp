92dd2a09b42ae0dfe58453a09ceccf3ca7d546c5
Oriaf <arox99@hotmail.se>
2021-07-06 14:50:30 +0200

    Removed the segfault when closing

--- game.cpp
+++ game.cpp
@@ -1335,7 +1335,7 @@ bool loadLevel(vector<GameObject *>* objects, Media* media,
 	objects->push_back(bRoom2Door);
 	objects->push_back(bRoom3Door);
 	objects->push_back(bRoom4Door);
-	objects->push_back(kitchenDoor);
+	//objects->push_back(kitchenDoor);
 	objects->push_back(kitchenDoor);
 	for(IsClickable *d : doors){
 		objects->push_back(dynamic_cast<GameObject*>((Room*)d));
